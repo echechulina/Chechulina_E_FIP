2e1d16cc79fe01d6d8f5693a12accad0f479e4ad
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-10-18 20:54:01 -0400

    adding content

--- js/main.js
+++ js/main.js
@@ -12,6 +12,35 @@
 
 	}
 
-	button.addEventListener("click", hamburgerMenu);
+    button.addEventListener("click", hamburgerMenu);
+    
+    
+
+     // make an AJAX request using FETCH API
+    
+    fetch('./data/classData.json')
+    .then(res => res.json())
+    .then(data => {
+    
+        // debugger;
+        
+        console.log(data);
+    
+        handleData(data);
+    })
+    
+        function handleData(data) {
+    
+            let websiteName = document.querySelector('.websiteName'),
+            position = document.querySelector('.position'),
+            info = document.querySelector('.info');
+
+                websiteName.innerHTML = data.websiteName;
+                position.innerHTML = data.position;
+                info.innerHTML = data.info
+    
+        }
+    
+  
 
 })();
