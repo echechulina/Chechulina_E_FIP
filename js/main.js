c992d1c4d7f919af32677ff1736e20e7dc7c1315
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-10-18 21:54:50 -0400

    adding styles for data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 (() =>{
 	
-	console.log("fired");
 
 	let button = document.querySelector("#button");
 	 burgerCon = document.querySelector("#burgerCon");
@@ -32,15 +31,22 @@
         function handleData(data) {
     
             let websiteName = document.querySelector('.websiteName'),
-            position = document.querySelector('.position'),
-            info = document.querySelector('.info');
+            positionOne = document.querySelector('.position_1'),
+            positionTwo = document.querySelector('.position_2'),
+            info_1 = document.querySelector('.info_1'),
+            info_2 = document.querySelector('.info_2'),
+            info_3 = document.querySelector('.info_3');
 
                 websiteName.innerHTML = data.websiteName;
-                position.innerHTML = data.position;
-                info.innerHTML = data.info
+                positionOne.innerHTML = data.position_1;
+                positionTwo.innerHTML = data.position_2;
+                info_1.innerHTML = data.info_1;
+                info_2.innerHTML = data.info_2;
+                info_3.innerHTML = data.info_3;
     
         }
     
-  
+
+
 
 })();
