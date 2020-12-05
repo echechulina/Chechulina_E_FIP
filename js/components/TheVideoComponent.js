3accd2c06e6340dd490d26d9b14e6fb33d9b140e
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-12-04 21:39:25 -0500

    adding video

--- js/components/TheVideoComponent.js
+++ js/components/TheVideoComponent.js
@@ -6,7 +6,7 @@ export default {
     template: `
          <section class="video-player">
             <i class="fas fa-film media-icon"></i></i><h1>This is the video component</h1>
-            <video :src="'video/' = work.source" controls></video>
+            <video :src="'video/' + work.source" controls></video>
             <h1>{{work.name}}</h1>
             <p>{{work.description}}</p>
         </section>`
